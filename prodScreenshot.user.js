dc77294f118b2aef8e5698dbe5b24c94f417794a
Tigrou Ind <tigrou.ind@gmail.com>
2020-07-31 21:46:31 +0200

    Only fetch background when image is visible

--- prodScreenshot.user.js
+++ prodScreenshot.user.js
@@ -7,6 +7,7 @@
 // @include http://www.pouet.net/*
 // @exclude http://www.pouet.net/
 // @exclude http://www.pouet.net/index.php*
+// @run-at document-end
 // ==/UserScript==
 /*
  * 2012.08.24 Tigrou^ind ( tigrou.ind@gmail.com ). Original idea and implementation
@@ -19,7 +20,7 @@
     // grab all the links to a prod
     var links = document.querySelectorAll('a[href*="prod.php?which="]');
 
-    for( var i=0, j=0, link=null; link=links[i++]; prev=link)
+    for( var i=0, link=null; link=links[i++]; prev=link)
     {
         // look for the prod_id and make sure it's different than the previous link
         var id = link.href!=prev.href && (link.href.match(/prod.php\?which=(\d+)$/)||['']).pop();
@@ -32,6 +33,24 @@
         s.style.cssText='display:inline-block;vertical-align:middle;width:100px;height:75px;margin:4px 4px 0 0;'+
             'background:no-repeat center center;background-size:contain;'
         link.insertBefore(s,link.firstChild);
-        setTimeout((function(s,id){s.style.backgroundImage=ext.map(function(v){ return 'url("/screenshots/'+id+'.'+v+'")'});}).bind(this,s,id), ++j*50);
+        s.setAttribute('prodid', id);
     }
+
+    //only try to load background when visible
+    var refresh_handler = function(e) {
+		var elements = document.querySelectorAll('span[prodid]');
+        for (var i = 0, j=0; i < elements.length; i++) {
+            var s = elements[i];
+            var rect = s.getBoundingClientRect();
+            if (rect.top < window.innerHeight && rect.bottom > 0) {
+                var id = s.getAttribute('prodid');
+                s.removeAttribute('prodid');
+                setTimeout((function(s,id){s.style.backgroundImage=ext.map(function(v){ return 'url("/screenshots/'+id+'.'+v+'")'});}).bind(this,s,id), ++j*50);
+            }
+        }
+    };
+
+    window.addEventListener('scroll', refresh_handler);
+    window.addEventListener('load', refresh_handler);
+    window.addEventListener('resize', refresh_handler);
 })();
